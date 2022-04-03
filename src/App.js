f037d4db78ab198a84822d9ec3a132bff28d8af5
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-04-03 21:24:32 +0600

    create route path in app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Reviews from './Components/Reviews/Reviews';
+import Chart from './Components/Chart/Chart';
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/chart' element={<Chart></Chart>}></Route>
+        <Route path='blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
